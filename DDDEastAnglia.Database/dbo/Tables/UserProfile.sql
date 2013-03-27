eedf983c21a895b7eea3fdbf3d5363d2c445680e
Alastair Smith <alastair.smith@grantadesign.com>
2013-03-27 12:39:55 +0000

    Add database project.
    Add a SQL Server Data Tools project for the database schema, including an
    import of the schema from the MDF file.

---
+++ DDDEastAnglia.Database/dbo/Tables/UserProfile.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[UserProfile] (
+    [UserId]        INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]      NVARCHAR (MAX) NULL,
+    [Name]          NVARCHAR (MAX) NOT NULL,
+    [EmailAddress]  NVARCHAR (MAX) NOT NULL,
+    [WebsiteUrl]    NVARCHAR (MAX) NULL,
+    [TwitterHandle] NVARCHAR (MAX) NULL,
+    [Bio]           NVARCHAR (MAX) NULL,
+    [MobilePhone]   NVARCHAR (MAX) NULL,
+    [NewSpeaker]    BIT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
