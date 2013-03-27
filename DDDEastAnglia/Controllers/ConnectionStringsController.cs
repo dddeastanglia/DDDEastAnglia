2e538a0669f827000d14cddc824f664522ba4071
Phil Pursglove <phil@philippursglove.com>
2013-03-27 16:06:11 +0000

    Added COnnectionStringsController and view

---
+++ DDDEastAnglia/Controllers/ConnectionStringsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace DDDEastAnglia.Controllers
+{
+    public class ConnectionStringsController : Controller
+    {
+        //
+        // GET: /ConnectionStrings/
+
+        public ActionResult Index()
+        {
+            Dictionary<string, string> connectionStrings = new Dictionary<string, string>();
+
+            foreach (dynamic connectionString in WebConfigurationManager.ConnectionStrings)
+            {
+                connectionStrings.Add(connectionString.Name, connectionString.ConnectionString);
+            }
+
+            return View(connectionStrings);
+        }
+
+    }
+}
