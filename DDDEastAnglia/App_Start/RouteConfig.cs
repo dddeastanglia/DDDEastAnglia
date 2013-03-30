427ba3b6fe1bbce5d6f2d7f88a015ae6318ada59
Ian Johnson <ij203@ninjaferret.co.uk>
2013-03-30 12:12:03 +0000

    Added namespaces to the route configs so that we do not have a situation where the routing engine gets confused between the two session controllers

--- DDDEastAnglia/App_Start/RouteConfig.cs
+++ DDDEastAnglia/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace DDDEastAnglia
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "DDDEastAnglia.Controllers" }
             );
         }
     }
