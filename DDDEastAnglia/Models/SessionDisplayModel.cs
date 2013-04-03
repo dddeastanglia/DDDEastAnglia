effa773ac03e24bf552657dfe6e9ba40bcb29fdc
Phil Pursglove <phil@philippursglove.com>
2013-04-03 23:07:08 +0100

    Hides submitted sessions until voting opens

--- DDDEastAnglia/Models/SessionDisplayModel.cs
+++ DDDEastAnglia/Models/SessionDisplayModel.cs
@@ -16,6 +16,8 @@ namespace DDDEastAnglia.Models
     public class SessionIndexModel
     {
         public bool IsOpenForSubmission { get; set; }
+        public bool IsOpenForVoting { get; set; }
+
         public IEnumerable<SessionDisplayModel> Sessions { get; set; }
     }
 }
