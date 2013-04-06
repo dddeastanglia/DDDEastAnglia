39e6d56f0e2a276529dddf9af51d51d45b2b557f
Alastair Smith <github@alastairsmith.me.uk>
2013-04-06 22:36:03 +0100

    Attempt to fix the include of the TeamCity module.
    Perhaps using a full path will fix it on TeamCity? Weirdly, the
    teamcity.psm1 file is opened in an editor on my machine when I run this.
    All a bit strange.

--- build/build.ps1
+++ build/build.ps1
@@ -2,7 +2,7 @@ properties {
 	$root = $null
 }
 
-Include "teamcity.psm1"
+Include .\teamcity.psm1
 TaskSetup {
     TeamCity-ReportBuildProgress "Running task $($psake.context.Peek().currentTaskName)"
 }
