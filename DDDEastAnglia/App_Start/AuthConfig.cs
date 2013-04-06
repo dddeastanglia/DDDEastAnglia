e3ca3739bf271b1af40b0fccdeea8155333dd261
Adrian Banks <github@adrianbanks.co.uk>
2013-04-07 00:11:59 +0100

    add a note for future editors

--- DDDEastAnglia/App_Start/AuthConfig.cs
+++ DDDEastAnglia/App_Start/AuthConfig.cs
@@ -10,6 +10,9 @@ namespace DDDEastAnglia
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 
+            // NOTE: if you add/edit this list, be sure to edit the OAuth provider links in 
+            //          the menu and the registration page as they don't get picked up automatically
+
             OAuthWebSecurity.RegisterClient(new GitHubOAuthClient(
                 WebConfigurationManager.AppSettings["GitHubAppId"],
                 WebConfigurationManager.AppSettings["GitHubSecret"]), "GitHub", null);
