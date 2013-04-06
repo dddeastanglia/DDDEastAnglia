0b1add1ebdec2c1ea7423b9f74c164574172d077
Alastair Smith <github@alastairsmith.me.uk>
2013-04-06 16:41:34 +0100

    Run builds in parallel.

--- build/build.ps1
+++ build/build.ps1
@@ -8,11 +8,11 @@ Task default -Depends Compile
 
 Task Compile -Depends Clean {
     Write-Host "Building DDDEastAnglia.sln" -ForegroundColor Green
-	Exec { msbuild "${root}\DDDEastAnglia.sln" /t:Build /p:Configuration=Release }
+	Exec { msbuild "${root}\DDDEastAnglia.sln" /t:Build /p:Configuration=Release /m }
 }
 
 Task Clean {
     Write-Host "Cleaning DDDEastAnglia.sln" -ForegroundColor Green
-    Exec { msbuild "${root}\DDDEastAnglia.sln" /t:Clean }	
+    Exec { msbuild "${root}\DDDEastAnglia.sln" /t:Clean /m }
 }
 
