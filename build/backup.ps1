acd2d1fa8a5f7a495e750968bee07a8010e9a8f9
Alastair Smith <github@alastairsmith.me.uk>
2013-04-10 20:56:32 +0100

    Clean up existing backups; create the destination.
    Remove any existing backups: these can be published on TeamCity as
    artefacts, or simply copied to a new destination if run manually.
    Discard the output of new-item so it doesn't clutter up the console.
    
    Add a few comments to the script to describe what it's doing.

--- build/backup.ps1
+++ build/backup.ps1
@@ -18,8 +18,21 @@ Task default -Depends Backup
 
 Task Backup {
     Write-Host "Backing up the DDDEastAnglia from Azure" -ForegroundColor Green
+
     $dateAndTime = Get-Date -Format "yyyy-MM-dd-HH-mm"
     $backupLocation = "$root\backups"
+
+    # Delete any existing directory at the backup location, and create a fresh one.
+
+    if (Test-Path $backupLocation -PathType Container) 
+    {
+        Remove-Item -Recurse -Force $backupLocation
+    }
+
+    New-Item $backupLocation -ItemType Container | Out-Null # discard the output object representing the created directory: stops clutter of the console.
+
+    # And now we can actually run the backup. Export the database to a BACPAC file
+    # (a deployment artefact encompassing both schema and data).
 
     Exec { 
         & "$root\build\targets\SSDT\SqlPackage.exe" /action:export `
