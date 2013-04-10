499563fca35569bcaca1b0a39ef28b153b979c67
Ian Johnson <ij203@ninjaferret.co.uk>
2013-04-10 11:38:42 +0100

    Adding to the voting to it's own controller and moving the retrieval and setting to a repository

---
+++ DDDEastAnglia/Controllers/VoteController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Mvc;
+using DDDEastAnglia.DataAccess;
+using DDDEastAnglia.Models;
+
+namespace DDDEastAnglia.Controllers
+{
+    public class VoteController : Controller
+    {
+        private readonly VotingCookieRepository _votingCookieRepository;
+
+        public VoteController() : this(new VotingCookieRepository())
+        {
+            
+        }
+
+        public VoteController(VotingCookieRepository votingCookieRepository)
+        {
+            _votingCookieRepository = votingCookieRepository;
+        }
+
+        public ActionResult RegisterVote(int id)
+        {
+            var cookie = _votingCookieRepository.Get(Request, VotingCookie.CookieName);
+            cookie.Add(id);
+            _votingCookieRepository.Save(Response, cookie);
+            return RedirectToAction("Index", "Session");
+        }
+
+        public ActionResult RemoveVote(int id)
+        {
+            var cookie = _votingCookieRepository.Get(Request, VotingCookie.CookieName);
+            cookie.Remove(id);
+            _votingCookieRepository.Save(Response, cookie);
+            return RedirectToAction("Index", "Session");
+        }
+    }
+}
---
+++ DDDEastAnglia/DataAccess/VotingCookieRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using DDDEastAnglia.Models;
+
+namespace DDDEastAnglia.DataAccess
+{
+    public class VotingCookieRepository
+    {
+        public VotingCookie Get(HttpRequestBase request, string cookieName)
+        {
+            var cookie = request.Cookies[VotingCookie.CookieName];
+            if (cookie == null)
+            {
+                return new VotingCookie(cookieName);
+            }
+            var sessionList = cookie.Value;
+            if (string.IsNullOrWhiteSpace(sessionList))
+            {
+                return new VotingCookie(cookieName, cookie.Expires);
+            }
+            var sessionIds = sessionList
+                                .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                                .Select(TryParse)
+                                .Where(val => val.HasValue)
+                                .Select(val => val.Value);
+            return new VotingCookie(cookieName, sessionIds, cookie.Expires);
+        }
+
+        public void Save(HttpResponseBase response, VotingCookie cookie)
+        {
+            var listOfSessions = string.Join(",", cookie.SessionsVotedFor.ToArray());
+            var httpCookie = new HttpCookie(cookie.Name, listOfSessions);
+            httpCookie.Expires = cookie.Expires;
+            if (response.Cookies[httpCookie.Name] != null)
+            {
+                response.Cookies.Set(httpCookie);
+            }
+            else
+            {
+                response.Cookies.Add(httpCookie);
+            }
+        }
+
+        private static int? TryParse(string value)
+        {
+            int val;
+            if (int.TryParse(value, out val))
+            {
+                return val;
+            }
+            return null;
+        }
+    }
+}
