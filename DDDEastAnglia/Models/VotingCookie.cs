e3f21a919630c97fa49a3b49ec6633876190b308
Ian Johnson <ij203@ninjaferret.co.uk>
2013-04-10 11:38:42 +0100

    Adding to the voting to it's own controller and moving the retrieval and setting to a repository

--- DDDEastAnglia/Models/VotingCookie.cs
+++ DDDEastAnglia/Models/VotingCookie.cs
@@ -10,25 +10,27 @@ namespace DDDEastAnglia.Models
         public const string CookieName = "DDDEA2013.Voting";
         private static readonly DateTime DefaultExpiry = new DateTime(2013, 4, 30);
         private readonly List<int> _sessionsVotedFor = new List<int>();
-        private DateTime _expires;
 
-        public VotingCookie()
-            : this(DefaultExpiry)
+        public VotingCookie(string name)
+            : this(name, DefaultExpiry)
         {
             
         }
 
-        public VotingCookie(DateTime expires)
+        public VotingCookie(string name, DateTime expires)
         {
-            _expires = expires;
+            Name = name;
+            Expires = expires;
         }
 
-        public VotingCookie(IEnumerable<int> sessionsVotedFor, DateTime expires) 
-            : this(expires)
+        public VotingCookie(string name, IEnumerable<int> sessionsVotedFor, DateTime expires) 
+            : this(name, expires)
         {
             _sessionsVotedFor.AddRange(sessionsVotedFor);
         }
 
+        public string Name { get; set; }
+        public DateTime Expires { get; set; }
         public IEnumerable<int> SessionsVotedFor { get { return _sessionsVotedFor.AsReadOnly(); } }
 
 
@@ -53,51 +55,6 @@ namespace DDDEastAnglia.Models
                 return;
             }
             _sessionsVotedFor.Remove(sessionId);
-        }
-
-        public void Save(HttpResponseBase response)
-        {
-            var listOfSessions = string.Join(",", _sessionsVotedFor.ToArray());
-            var cookie = new HttpCookie(CookieName, listOfSessions);
-            cookie.Expires = _expires;
-            if (response.Cookies[CookieName] != null)
-            {
-                response.Cookies.Set(cookie);
-            }
-            else
-            {
-                response.Cookies.Add(cookie);
-            }
-        }
-
-        public static VotingCookie Get(HttpRequestBase request)
-        {
-            var cookie = request.Cookies[CookieName];
-            if (cookie == null)
-            {
-                return new VotingCookie();
-            }
-            var sessionList = cookie.Value;
-            if (string.IsNullOrWhiteSpace(sessionList))
-            {
-                return new VotingCookie(cookie.Expires);
-            }
-            var sessionIds = sessionList
-                                .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
-                                .Select(TryParse)
-                                .Where(val => val.HasValue)
-                                .Select(val => val.Value);
-            return new VotingCookie(sessionIds, cookie.Expires);
-        }
-
-        private static int? TryParse(string value)
-        {
-            int val;
-            if (int.TryParse(value, out val))
-            {
-                return val;
-            }
-            return null;
         }
     }
 }
