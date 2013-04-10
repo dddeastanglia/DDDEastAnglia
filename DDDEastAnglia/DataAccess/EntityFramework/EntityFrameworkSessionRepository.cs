a81dbdca939318aceaef7b116b773b8c8e8fbdae
Ian Johnson <ij203@ninjaferret.co.uk>
2013-04-10 23:56:39 +0100

    Adding some more tests for the voting pages

---
+++ DDDEastAnglia/DataAccess/EntityFramework/EntityFrameworkSessionRepository.cs
@@ -0,0 +1,19 @@
+﻿using DDDEastAnglia.Models;
+
+namespace DDDEastAnglia.DataAccess.EntityFramework
+{
+    public class EntityFrameworkSessionRepository : ISessionRepository
+    {
+        private readonly DDDEAContext context = new DDDEAContext();
+
+        public Session Get(int id)
+        {
+            return context.Sessions.Find(id);
+        }
+
+        public bool Exists(int id)
+        {
+            return context.Sessions.Find(id) != null;
+        }
+    }
+}
