29725a5ce35f38f1953b059321b9923def62cdd5
Ian Johnson <ij203@ninjaferret.co.uk>
2013-04-12 00:35:27 +0100

    Adding more functionality for voting

---
+++ DDDEastAnglia.Tests/Given_That_I_Have_Voted_For_Several_Sessions_The_VoteController_Should.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace DDDEastAnglia.Tests
+{
+    [TestFixture]
+    public class Given_That_I_Have_Voted_For_Several_Sessions_The_VoteController_Should
+    {
+        
+    }
+}
