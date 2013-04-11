d8f3336f35d46a183e87b3a6de60df0c4937b08f
Ian Johnson <ij203@ninjaferret.co.uk>
2013-04-12 00:35:27 +0100

    Adding more functionality for voting

---
+++ DDDEastAnglia/DataAccess/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace DDDEastAnglia.DataAccess
+{
+    public interface IRepository<TEntity, in TIdentifier>
+    {
+        TEntity Get(TIdentifier identifier);
+        void Update(TEntity entity);
+        void Save(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(TIdentifier identifier);
+        bool Exists(TIdentifier identifier);
+    }
+}
