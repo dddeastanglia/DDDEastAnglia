6554596df7e68aa8939a8ea3c4c549b343ba7f65
Ian Johnson <ij203@ninjaferret.co.uk>
2013-04-13 00:51:43 +0100

    Adding more tests around the voting feature and also adding the ability to render the voting controls

---
+++ DDDEastAnglia.Tests/EventHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DDDEastAnglia.DataModel;
+
+namespace DDDEastAnglia.Tests
+{
+    public class EventHelper
+    {
+        public static Event BuildEvent(bool isOpenForSubmission, bool isOpenForVoting)
+        {
+            var submissionStartDate = isOpenForSubmission ? DateTime.Now.AddDays(-3) : new DateTime(2013, 1, 1);
+            var submissionEndDate = isOpenForSubmission ? DateTime.Now.AddDays(3) : new DateTime(2013, 1, 31);
+            var votingStartDate = isOpenForVoting ? DateTime.Now.AddDays(-3) : new DateTime(2013, 1, 1);
+            var votingEndDate = isOpenForVoting ? DateTime.Now.AddDays(3) : new DateTime(2013, 1, 31);
+            return new Event
+            {
+                Name = "DDD East Anglia 2013",
+                ShortName = "DDDEA2013",
+                Visible = true,
+                PreConferenceAgenda = new PreConferenceAgenda(new[]
+                        {
+                            new PreConferenceAgendaItem{DateType = DateType.SubmissionStarts, Date = submissionStartDate},
+                            new PreConferenceAgendaItem{DateType = DateType.SubmissionEnds, Date = submissionEndDate},
+                            new PreConferenceAgendaItem{DateType = DateType.VotingStarts, Date = votingStartDate},
+                            new PreConferenceAgendaItem{DateType = DateType.VotingEnds, Date = votingEndDate},
+                            new PreConferenceAgendaItem{DateType = DateType.AgendaPublished, Date = new DateTime(2013, 5, 29)},
+                            new PreConferenceAgendaItem{DateType = DateType.RegistrationOpens, Date = new DateTime(2013, 6, 1)},
+                            new PreConferenceAgendaItem{DateType = DateType.EventStarts, Date = new DateTime(2013, 6, 29, 8, 30, 0)},
+                            new PreConferenceAgendaItem{DateType = DateType.EventEnds, Date = new DateTime(2013, 6, 29, 18, 0, 0)},
+                        })
+            };
+        } 
+    }
+}
---
+++ DDDEastAnglia.Tests/VoteHelper.cs
@@ -0,0 +1,43 @@
+﻿using DDDEastAnglia.DataModel;
+using NUnit.Framework;
+
+namespace DDDEastAnglia.Tests
+{
+    public static class VoteHelper
+    {
+        public static bool IsVoteFor(this Vote vote, string eventId, int expectedSessionId)
+        {
+            if (!vote.Event.Equals(eventId))
+            {
+                return false;
+            }
+            if (vote.SessionId != expectedSessionId)
+            {
+                return false;
+            }
+            if (!vote.IsVote)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool VoteHasBeenRemoved(this Vote vote, string eventId, int sessionRemoved)
+        {
+            if (!vote.Event.Equals(eventId))
+            {
+                return false;
+            }
+            if (vote.SessionId != sessionRemoved)
+            {
+                return false;
+            }
+            if (vote.IsVote)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
