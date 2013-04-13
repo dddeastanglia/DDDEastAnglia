bc3c2a4430f6f674fba9381b5679be60f173dba5
Ian Johnson <ij203@ninjaferret.co.uk>
2013-04-13 17:12:12 +0100

    Moved the tests into a voting directory, made them inherit from a common base and added a test to show the saving of the IPAddress

--- DDDEastAnglia.Tests/Voting/CookieHelper.cs
+++ DDDEastAnglia.Tests/Voting/CookieHelper.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using DDDEastAnglia.Models;
-using NUnit.Framework;
 
-namespace DDDEastAnglia.Tests
+namespace DDDEastAnglia.Tests.Voting
 {
     public static class CookieHelper
     {
---
+++ DDDEastAnglia.Tests/Voting/Given_That_I_Am_Removing_A_Vote_The_VoteController_Should.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using DDDEastAnglia.DataAccess;
+using DDDEastAnglia.DataModel;
+using DDDEastAnglia.Models;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DDDEastAnglia.Tests.Voting
+{
+    [TestFixture]
+    public class Given_That_I_Am_Removing_A_Vote_The_VoteController_Should : VotingTestBase
+    {
+        private const int KnownSessionId = 1;
+        private static readonly int[] SessionsVotedFor = new[] {KnownSessionId};
+        private VotingCookie cookieWithNoVotes;
+
+        protected override void SetCookieRepositoryExpectations(IVotingCookieRepository repository)
+        {
+            base.SetCookieRepositoryExpectations(repository);
+            cookieWithNoVotes = new VotingCookie(Guid.NewGuid(), VotingCookie.CookieName, SessionsVotedFor, new DateTime(2013, 4, 30));
+            repository.Get(Arg.Is(cookieWithNoVotes.Name))
+                            .Returns(cookieWithNoVotes);
+        }
+
+        protected override void SetSessionRepositoryExpectations(ISessionRepository sessionRepository)
+        {
+            base.SetSessionRepositoryExpectations(sessionRepository);
+            sessionRepository.Exists(Arg.Is(KnownSessionId)).Returns(true);
+        }
+
+        [Test]
+        public void Record_The_EventID()
+        {
+            Controller.RemoveVote(KnownSessionId);
+
+            VoteRepository.Received().Save(Arg.Is<Vote>(vote => vote.Event == "DDDEA2013"));
+        }
+
+        [Test]
+        public void Record_The_SessionId()
+        {
+            Controller.RemoveVote(KnownSessionId);
+
+            VoteRepository.Received().Save(Arg.Is<Vote>(vote => vote.SessionId == KnownSessionId));
+        }
+
+        [Test]
+        public void Record_The_Cookie_Guid()
+        {
+            Controller.RemoveVote(KnownSessionId);
+
+            VoteRepository.Received().Save(Arg.Is<Vote>(vote => vote.CookieId.Equals(cookieWithNoVotes.Id)));
+        }
+
+        [Test]
+        public void Record_The_Time_Of_The_Vote()
+        {
+            Controller.RemoveVote(KnownSessionId);
+
+            VoteRepository.Received().Save(Arg.Is<Vote>(vote => vote.TimeRecorded == SimulatedNow));
+        }
+
+        [Test]
+        public void Record_That_The_Vote_Is_Actually_A_Vote()
+        {
+            Controller.RemoveVote(KnownSessionId);
+
+            VoteRepository.Received().Save(Arg.Is<Vote>(vote => !vote.IsVote));
+        }
+    }
+}
