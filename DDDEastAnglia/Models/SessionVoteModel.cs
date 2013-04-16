0895afb0b12bc9be9de92b7571c53a538d9ac0a7
Ian Johnson <ij203@ninjaferret.co.uk>
2013-04-16 21:09:36 +0100

    If voting is not open then make the Status method of the VoteController return the EmptyResult rather than rendering a view

--- DDDEastAnglia/Models/SessionVoteModel.cs
+++ DDDEastAnglia/Models/SessionVoteModel.cs
@@ -3,7 +3,6 @@
     public class SessionVoteModel
     {
         public int SessionId { get; set; }
-        public bool UserCanVote { get; set; }
         public bool VotedForByUser { get; set; }
     }
 }
