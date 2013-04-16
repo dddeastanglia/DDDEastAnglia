d8ed23f0fdd72f5baf8a3ef363a3b37ea3e1ac5a
Ian Johnson <ij203@ninjaferret.co.uk>
2013-04-16 20:48:45 +0100

    Renaming the Vote property on DDDEAContext to Votes for consistency

--- DDDEastAnglia/DataAccess/EntityFramework/EntityFrameworkVoteRepository.cs
+++ DDDEastAnglia/DataAccess/EntityFramework/EntityFrameworkVoteRepository.cs
@@ -10,18 +10,18 @@ namespace DDDEastAnglia.DataAccess.EntityFramework
 
         public void Save(Vote vote)
         {
-            context.Vote.Add(vote);
+            context.Votes.Add(vote);
             context.SaveChanges();
         }
 
         public void Delete(int sessionId, Guid cookieId)
         {
-            var toDelete = context.Vote.FirstOrDefault(vote => vote.SessionId == sessionId && vote.CookieId == cookieId);
+            var toDelete = context.Votes.FirstOrDefault(vote => vote.SessionId == sessionId && vote.CookieId == cookieId);
             if (toDelete == null)
             {
                 return;
             }
-            context.Vote.Remove(toDelete);
+            context.Votes.Remove(toDelete);
             context.SaveChanges();
         }
     }
