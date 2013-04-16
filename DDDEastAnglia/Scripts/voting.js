5aed2d135b877f72eedc1202785154e07a5bc13a
Ian Johnson <ij203@ninjaferret.co.uk>
2013-04-16 20:58:51 +0100

    Add the voting.js file

---
+++ DDDEastAnglia/Scripts/voting.js
@@ -0,0 +1,29 @@
+﻿
+$(function () {
+    $('.voting').each(function(index) {
+        replaceSubmit($(this), onVoteComplete($(this)));
+    });
+    
+    function replaceSubmit(voteSpan, handler) {
+        voteSpan.find('form').submit(function (e) {
+            e.preventDefault();
+            var data = { width: $(window).width(), height: $(window).height() };
+
+            // Now I want to take the form's url and post directly to it if successful then change the style of the button
+            $.post(
+                this.action,
+                data,
+                handler
+            );
+        });
+    }
+    
+    function onVoteComplete(voteSpan) {
+        return function (response) {
+            var id = voteSpan.attr('id');
+            voteSpan.replaceWith(response);
+            var newItem = $('#' + id);
+            replaceSubmit(newItem, onVoteComplete(newItem));
+        };
+    }
+});
