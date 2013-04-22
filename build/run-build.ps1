4b7bf7b198e45b3e0e5b23768cd5f5f2effadf55
Alastair Smith <github@alastairsmith.me.uk>
2013-04-22 20:40:49 +0100

    Add new environment script to set PATH, etc.
    And call the script from the run-build script.

--- build/run-build.ps1
+++ build/run-build.ps1
@@ -1,3 +1,6 @@
 $rootDir = join-path (Split-Path $MyInvocation.MyCommand.Path) ".."
+
+& "${rootDir}\build\environment.ps1" $rootDir
+
 Import-Module (join-path $rootDir "psake\psake.psm1")
 invoke-psake -framework '4.0' (join-path $rootDir "build\build.ps1") -properties @{"root"="$rootDir";}
