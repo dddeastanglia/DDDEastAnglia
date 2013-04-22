1c9998b794c40ca3426a7d3b3effe5c607f18e39
Alastair Smith <github@alastairsmith.me.uk>
2013-04-22 23:30:15 +0100

    Fix the admin User controller.

--- DDDEastAnglia/Areas/Admin/Controllers/UserController.cs
+++ DDDEastAnglia/Areas/Admin/Controllers/UserController.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using DDDEastAnglia.DataAccess;
 using DDDEastAnglia.DataAccess.EntityFramework;
 using DDDEastAnglia.Models;
 
 namespace DDDEastAnglia.Areas.Admin.Controllers
 {
     [Authorize(Roles = "Administrator")]
-    public partial class UserController : Controller
+    public class UserController : Controller
     {
         private readonly DDDEAContext db = new DDDEAContext();
         
         // GET: /Admin/User/
-        public virtual ActionResult Index()
+        public ActionResult Index()
         {
             List<UserProfile> profiles = db.UserProfiles.ToList();
             return null;
