cc410d4ef2c148802d757cbe9148f8a0f1bd64d4
Alastair Smith <github@alastairsmith.me.uk>
2013-04-23 11:41:29 +0100

    Fix #104: Add a Test task to the build script.
    Execute the unit tests via nunit-console.

--- build/build.ps1
+++ build/build.ps1
@@ -7,11 +7,24 @@ TaskSetup {
     TeamCity-ReportBuildProgress "Running task '$($psake.context.Peek().currentTaskName)'"
 }
 
-Task default -Depends Compile
+Task default -Depends Test
+
+Task Test -Depends Compile {
+    $nunitDir = Join-Path ((ls "${root}\packages\*" -Filter NUnit.Runners*) | select -last 1) "tools"
+    $env:Path = $nunitDir + ";" + $env:Path
+
+    $testAssemblies = ls -Recurse "${root}\DDDEastAnglia*\bin" -Filter "DDDEastAnglia*Tests.dll"
+
+    Write-Host "Running tests" -ForegroundColor Green
+    Exec { nunit-console $testAssemblies /framework="4.0" /nologo /nodots /result="${root}\TestResult.xml" /process="Separate" }
+}
 
 Task Compile -Depends Clean {
+    Write-Host "Restoring Solution-level NuGet packages" -ForegroundColor Green	
+    Exec { nuget install "${root}\.nuget\packages.config" -OutputDirectory "${root}\packages" }
+
     Write-Host "Building DDDEastAnglia.sln" -ForegroundColor Green
-	Exec { msbuild "${root}\DDDEastAnglia.sln" /t:Build /p:Configuration=Release /m }
+    Exec { msbuild "${root}\DDDEastAnglia.sln" /t:Build /p:Configuration=Release /m }
 }
 
 Task Clean {
