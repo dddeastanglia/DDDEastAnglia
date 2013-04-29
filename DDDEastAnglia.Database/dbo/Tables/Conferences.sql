aaca99650f943e094df6e50bf98c04dceb2b9bdf
Alastair Smith <github@alastairsmith.me.uk>
2013-04-29 23:25:27 +0100

    Update Database Project with Voting changes.
    Add the new tables and fix the constraints in the Sessions table.

---
+++ DDDEastAnglia.Database/dbo/Tables/Conferences.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Conferences] (
+    [ConferenceId] INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (MAX) NOT NULL,
+    [ShortName]    NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_dbo.Conferences] PRIMARY KEY CLUSTERED ([ConferenceId] ASC)
+);
+
--- DDDEastAnglia.Database/dbo/Tables/Sessions.sql
+++ DDDEastAnglia.Database/dbo/Tables/Sessions.sql
@@ -4,6 +4,16 @@
     [Abstract]        NVARCHAR (MAX) NOT NULL,
     [SpeakerUserName] NVARCHAR (MAX) NULL,
     [Votes]           INT            NOT NULL,
-    CONSTRAINT [PK_dbo.Sessions] PRIMARY KEY CLUSTERED ([SessionId] ASC)
+    [ConferenceId]    INT            DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_dbo.Sessions] PRIMARY KEY CLUSTERED ([SessionId] ASC),
+    CONSTRAINT [FK_dbo.Sessions_dbo.Conferences_ConferenceId] FOREIGN KEY ([ConferenceId]) REFERENCES [dbo].[Conferences] ([ConferenceId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Session_Conferences] FOREIGN KEY ([ConferenceId]) REFERENCES [dbo].[Conferences] ([ConferenceId])
 );
+
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ConferenceId]
+    ON [dbo].[Sessions]([ConferenceId] ASC);
 
