dac425d1b06f2a3b4c331472b082e00cdccb0981
Simon P Stevens <simonpstevens@gmail.com>
2013-04-30 15:45:49 +0100

    Added random ordering of sessions when voting is active.

---
+++ DDDEastAnglia/Helpers/EnumerableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDDEastAnglia.Helpers
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Randomly shuffles the provided list.
+        /// All possible orders are equally likely.
+        /// Based on the Fisher-Yates shuffle. http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+        /// </summary>
+        public static void RandomShuffle<T>(this IList<T> data)
+        {
+            Random randomGen = new Random();
+            for (int i = data.Count - 1; i >= 1; i--)
+            {
+                // Randomly select items to move to the end of the list.
+                // After each iteration, the item just moved to the end
+                // will no longer be in the set of items that are
+                // available to swap.
+                int swapIndex = randomGen.Next(0, i + 1);
+
+                T swap = data[i];
+                data[i] = data[swapIndex];
+                data[swapIndex] = swap;
+            }
+        }
+    }
+}
