a00a9c2d7e1fbd29c37472d69886c1fddac410a6
Phil Pursglove <phil@philippursglove.com>
2013-05-03 16:01:50 +0100

    Role creation

--- DDDEastAnglia/Areas/Admin/Controllers/RoleController.cs
+++ DDDEastAnglia/Areas/Admin/Controllers/RoleController.cs
@@ -1,5 +1,6 @@
-﻿using System.Web.Mvc;
-using WebMatrix.WebData;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Security;
 
 namespace DDDEastAnglia.Areas.Admin.Controllers
 {
@@ -9,8 +10,50 @@ namespace DDDEastAnglia.Areas.Admin.Controllers
         // GET: /Admin/Role/
         public ActionResult Index()
         {
-            SimpleRoleProvider provider = new SimpleRoleProvider();
-            return null;
+            List<string> roles = new List<string>();
+            roles.AddRange(Roles.GetAllRoles());
+
+            return this.View(roles);
         }
+
+        // GET: /Admin/Role/Manage
+        public ActionResult Manage(string id)
+        {
+            return this.View();
+        }
+
+        // GET: /Admin/Role/Delete
+        public ActionResult Delete(string id)
+        {
+            return this.View();
+        }
+
+        // GET: /Admin/Role/Create
+        public ActionResult Create()
+        {
+            string role = string.Empty;
+
+            return this.View(role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(string role)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!Roles.RoleExists(role))
+                {
+                    Roles.CreateRole(role);
+                }
+                else
+                {
+                    ViewBag["Message"] = "This role already exists!";
+                    return this.View(role);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
