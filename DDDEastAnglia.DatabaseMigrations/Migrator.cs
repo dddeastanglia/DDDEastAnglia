f208ae66062d6f91ca7764fe0f827fe1321dc9fa
Adrian Banks <github@adrianbanks.co.uk>
2014-04-27 21:41:50 +0100

    add the new migrations project

---
+++ DDDEastAnglia.DatabaseMigrations/Migrator.cs
@@ -0,0 +1,45 @@
+﻿using FluentMigrator;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using FluentMigrator.Runner.Processors.SqlServer;
+
+namespace DDDEastAnglia.DatabaseMigrations
+{
+    public class Migrator
+    {
+        private readonly string connectionString;
+
+        public Migrator(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void MigrateTo(long targetVersion)
+        {
+            var factory = new SqlServer2012ProcessorFactory();
+            var announcer = new ConsoleAnnouncer
+                                    {
+                                        ShowElapsedTime = true,
+                                        ShowSql = true
+                                    };
+            var options = new MigrationOptions
+                                    {
+                                        PreviewOnly = false, 
+                                        Timeout = 60
+                                    };
+            var processor = factory.Create(connectionString, announcer, options);
+            var migrationContext = new RunnerContext(announcer) { Namespace = "DDDEastAnglia.DatabaseMigrations.Migrations" };
+
+            var runner = new MigrationRunner(GetType().Assembly, migrationContext, processor);
+            runner.MigrateUp(targetVersion, true);
+        }
+    }
+
+    public class MigrationOptions : IMigrationProcessorOptions
+    {
+        public bool PreviewOnly{get;set;}
+        public int Timeout{get;set;}
+        public string ProviderSwitches{get;set;}
+    }
+}
---
+++ DDDEastAnglia.DatabaseMigrations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DDDEastAnglia.DatabaseMigrations
+{
+    public class Program
+    {
+        internal static void Main()
+        {
+            string connectionString = @"Data Source=.;Initial Catalog=DDDEastAnglia-migration;Integrated Security=True";
+            var migrator = new Migrator(connectionString);
+            migrator.MigrateTo(20140101);
+
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+    }
+}
