229beb92ca1aaa75f5bbae65bc3f3ebcafebc617
Adrian Banks <github@adrianbanks.co.uk>
2014-04-29 23:22:10 +0100

    add some tests for hiding the voting stats link

---
+++ DDDEastAnglia.Tests/Admin/AdminHomeControllerTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Web.Mvc;
+using DDDEastAnglia.Areas.Admin.Controllers;
+using DDDEastAnglia.Areas.Admin.Models;
+using DDDEastAnglia.DataAccess;
+using DDDEastAnglia.Domain;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DDDEastAnglia.Tests.Admin
+{
+    [TestFixture]
+    public sealed class AdminHomeControllerTests
+    {
+        [Test]
+        public void VotingLink_ShouldBeHidden_ByDefault()
+        {
+            var conference = Substitute.For<IConference>();
+
+            var model = GetViewModel(conference);
+            
+            Assert.That(model.ShowVotingStatsLink, Is.False);
+        }
+
+        [Test]
+        public void VotingLink_ShouldBeHidden_WhenSessionSubmissionIsOpen()
+        {
+            var conference = Substitute.For<IConference>();
+            conference.CanSubmit().Returns(true);
+
+            var model = GetViewModel(conference);
+
+            Assert.That(model.ShowVotingStatsLink, Is.False);
+        }
+
+        [Test]
+        public void VotingLink_ShouldBeHidden_WhenVotingIsOpen()
+        {
+            var conference = Substitute.For<IConference>();
+            conference.CanVote().Returns(true);
+
+            var model = GetViewModel(conference);
+
+            Assert.That(model.ShowVotingStatsLink, Is.True);
+        }
+
+        [Test]
+        public void VotingLink_ShouldBeHidden_WhenTheAgendaIsPublished()
+        {
+            var conference = Substitute.For<IConference>();
+            conference.CanPublishAgenda().Returns(true);
+
+            var model = GetViewModel(conference);
+
+            Assert.That(model.ShowVotingStatsLink, Is.True);
+        }
+
+        [Test]
+        public void VotingLink_ShouldBeHidden_WhenRegistrationIsOpen()
+        {
+            var conference = Substitute.For<IConference>();
+            conference.CanRegister().Returns(true);
+
+            var model = GetViewModel(conference);
+
+            Assert.That(model.ShowVotingStatsLink, Is.True);
+        }
+
+        private MenuViewModel GetViewModel(IConference conference)
+        {
+            var conferenceLoader = Substitute.For<IConferenceLoader>();
+            conferenceLoader.LoadConference().Returns(conference);
+            var controller = new AdminHomeController(conferenceLoader);
+
+            var result = (ViewResult) controller.Index();
+            var model = (MenuViewModel) result.Model;
+            return model;
+        }
+    }
+}
