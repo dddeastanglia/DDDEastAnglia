df778b183307be27da1391b1ebecbdf287e22eae
Adrian Banks <github@adrianbanks.co.uk>
2014-05-12 23:14:31 +0100

    get the dates in the sidebar from the database

---
+++ DDDEastAnglia/Models/TimelineModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DDDEastAnglia.Models
+{
+    public class TimelineModel
+    {
+        public string SubmissionOpens{get;set;} 
+        public string SubmissionCloses{get;set;} 
+        public string VotingOpens{get;set;} 
+        public string VotingCloses{get;set;} 
+        public string AgendaAnnounced{get;set;} 
+        public string RegistrationOpens{get;set;} 
+    }
+}
