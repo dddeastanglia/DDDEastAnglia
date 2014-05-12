e08b2a25f8b2e2612e41fa6cf0b5332c618ced03
Adrian Banks <github@adrianbanks.co.uk>
2014-05-12 21:17:37 +0100

    add some tests to check that the complete list of speakers and sessions cannot be seen when not appropriate

---
+++ DDDEastAnglia.Tests/Controllers/SessionControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using DDDEastAnglia.Controllers;
+using DDDEastAnglia.DataAccess;
+using DDDEastAnglia.Domain;
+using DDDEastAnglia.Helpers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DDDEastAnglia.Tests.Controllers
+{
+    [TestFixture]
+    public sealed class SessionControllerTests
+    {
+        [Test]
+        public void CannotSeeSessionList_WhenTheConferenceSaysThatSessionsCannotBeShown()
+        {
+            var conference = Substitute.For<IConference>();
+            conference.CanShowSessions().Returns(false);
+            var conferenceLoader = Substitute.For<IConferenceLoader>();
+            conferenceLoader.LoadConference().Returns(conference);
+            var userProfileRepository = Substitute.For<IUserProfileRepository>();
+            var sessionRepository = Substitute.For<ISessionRepository>();
+            var sessionSorter = Substitute.For<ISessionSorter>();
+            var controller = new SessionController(conferenceLoader, userProfileRepository, sessionRepository, sessionSorter);
+
+            var result = controller.Index();
+
+            Assert.That(result, Is.InstanceOf<HttpNotFoundResult>());
+        }
+    }
+}
