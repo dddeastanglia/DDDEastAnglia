ec671d0828e55c64f6d456c351efb86979c185d1
Adrian Banks <github@adrianbanks.co.uk>
2014-05-13 22:33:48 +0100

    Extract the constructor of the controller into a helper method

--- DDDEastAnglia.Tests/Controllers/SessionControllerTests.cs
+++ DDDEastAnglia.Tests/Controllers/SessionControllerTests.cs
@@ -17,9 +17,7 @@ namespace DDDEastAnglia.Tests.Controllers
         {
             var conference = Substitute.For<IConference>();
             conference.CanShowSessions().Returns(false);
-            var conferenceLoader = Substitute.For<IConferenceLoader>();
-            conferenceLoader.LoadConference().Returns(conference);
-            var controller = new SessionController(conferenceLoader, Substitute.For<IUserProfileRepository>(), Substitute.For<ISessionRepository>(), Substitute.For<ISessionSorter>());
+            var controller = SessionControllerFactory.Create(conference);
 
             var result = controller.Index();
 
@@ -31,9 +29,7 @@ namespace DDDEastAnglia.Tests.Controllers
         {
             var conference = Substitute.For<IConference>();
             conference.CanSubmit().Returns(false);
-            var conferenceLoader = Substitute.For<IConferenceLoader>();
-            conferenceLoader.LoadConference().Returns(conference);
-            var controller = new SessionController(conferenceLoader, Substitute.For<IUserProfileRepository>(), Substitute.For<ISessionRepository>(), Substitute.For<ISessionSorter>());
+            var controller = SessionControllerFactory.Create(conference);
 
             var result = controller.Create(new Session());
 
@@ -43,10 +39,8 @@ namespace DDDEastAnglia.Tests.Controllers
         [Test]
         public void CannotBeginToEditASession_WhenTheSessionBelongsToAnotherUser()
         {
-            var session = new Session {SpeakerUserName = "bob"};
-            var sessionRepository = Substitute.For<ISessionRepository>();
-            sessionRepository.Get(1).Returns(session);
-            var controller = new SessionController(Substitute.For<IConferenceLoader>(), Substitute.For<IUserProfileRepository>(), sessionRepository, Substitute.For<ISessionSorter>());
+            var session = new Session { SessionId = 1, SpeakerUserName = "bob" };
+            var controller = SessionControllerFactory.Create(session);
 
             var result = controller.Edit("fred", 1);
 
@@ -57,9 +51,7 @@ namespace DDDEastAnglia.Tests.Controllers
         public void CannotSaveAnEditToASession_WhenTheSessionBelongsToAnotherUser()
         {
             var session = new Session {SessionId = 1, SpeakerUserName = "bob"};
-            var sessionRepository = Substitute.For<ISessionRepository>();
-            sessionRepository.Get(1).Returns(session);
-            var controller = new SessionController(Substitute.For<IConferenceLoader>(), Substitute.For<IUserProfileRepository>(), sessionRepository, Substitute.For<ISessionSorter>());
+            var controller = SessionControllerFactory.Create(session);
 
             var result = controller.Edit("fred", session);
 
@@ -70,9 +62,7 @@ namespace DDDEastAnglia.Tests.Controllers
         public void CannotBeginToDeleteASession_WhenTheSessionBelongsToAnotherUser()
         {
             var session = new Session { SessionId = 1, SpeakerUserName = "bob" };
-            var sessionRepository = Substitute.For<ISessionRepository>();
-            sessionRepository.Get(1).Returns(session);
-            var controller = new SessionController(Substitute.For<IConferenceLoader>(), Substitute.For<IUserProfileRepository>(), sessionRepository, Substitute.For<ISessionSorter>());
+            var controller = SessionControllerFactory.Create(session);
 
             var result = controller.Delete("fred", 1);
 
@@ -83,13 +73,30 @@ namespace DDDEastAnglia.Tests.Controllers
         public void CannotConfirmToDeleteASession_WhenTheSessionBelongsToAnotherUser()
         {
             var session = new Session { SessionId = 1, SpeakerUserName = "bob" };
-            var sessionRepository = Substitute.For<ISessionRepository>();
-            sessionRepository.Get(1).Returns(session);
-            var controller = new SessionController(Substitute.For<IConferenceLoader>(), Substitute.For<IUserProfileRepository>(), sessionRepository, Substitute.For<ISessionSorter>());
+            var controller = SessionControllerFactory.Create(session);
 
             var result = controller.DeleteConfirmed("fred", 1);
 
             Assert.That(result, Is.InstanceOf<HttpUnauthorizedResult>());
+        }
+
+        private static class SessionControllerFactory
+        {
+            public static SessionController Create(IConference conference)
+            {
+                var conferenceLoader = Substitute.For<IConferenceLoader>();
+                conferenceLoader.LoadConference().Returns(conference);
+                var controller = new SessionController(conferenceLoader, Substitute.For<IUserProfileRepository>(), Substitute.For<ISessionRepository>(), Substitute.For<ISessionSorter>());
+                return controller;
+            }
+
+            public static SessionController Create(Session session)
+            {
+                var sessionRepository = Substitute.For<ISessionRepository>();
+                sessionRepository.Get(session.SessionId).Returns(session);
+                var controller = new SessionController(Substitute.For<IConferenceLoader>(), Substitute.For<IUserProfileRepository>(), sessionRepository, Substitute.For<ISessionSorter>());
+                return controller;
+            }
         }
     }
 }
