71de93d7b6b7445ddeba5359b2361b0eb1b47379
Adrian Banks <github@adrianbanks.co.uk>
2014-06-17 22:18:10 +0100

    add a speakers page that shows just the users who have submitted sessions

---
+++ DDDEastAnglia/Areas/Admin/Controllers/SpeakerController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using DDDEastAnglia.Areas.Admin.Models;
+using DDDEastAnglia.DataAccess;
+using DDDEastAnglia.Models;
+
+namespace DDDEastAnglia.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class SpeakerController : Controller
+    {
+        private readonly IUserProfileRepository userProfileRepository;
+        private readonly ISessionRepository sessionRepository;
+
+        public SpeakerController(IUserProfileRepository userProfileRepository, ISessionRepository sessionRepository)
+        {
+            if (userProfileRepository == null)
+            {
+                throw new ArgumentNullException("userProfileRepository");
+            }
+
+            if (sessionRepository == null)
+            {
+                throw new ArgumentNullException("sessionRepository");
+            }
+            
+            this.userProfileRepository = userProfileRepository;
+            this.sessionRepository = sessionRepository;
+        }
+
+        public ActionResult Index()
+        {
+            var sessionCountsPerSpeaker = sessionRepository.GetAllSessions()
+                                                        .GroupBy(s => s.SpeakerUserName)
+                                                        .Where(g => g.Any())
+                                                        .ToDictionary(g => g.Key, g => g.Count());
+
+            var speakerUserNames = new HashSet<string>(sessionCountsPerSpeaker.Select(p => p.Key));
+
+            var speakers = userProfileRepository.GetAllUserProfiles()
+                                                .Where(u => speakerUserNames.Contains(u.UserName))
+                                                .Select(CreateUserModel)
+                                                .OrderBy(u => u.UserName).ToList();
+            foreach (var speaker in speakers)
+            {
+                int sessionCount;
+                sessionCountsPerSpeaker.TryGetValue(speaker.UserName, out sessionCount);
+                speaker.SubmittedSessionCount = sessionCount;
+            }
+
+            return View(speakers);
+        }
+    
+        private static SpeakerModel CreateUserModel(UserProfile profile)
+        {
+            return new SpeakerModel
+            {
+                UserId = profile.UserId,
+                UserName = profile.UserName,
+                Name = profile.Name,
+                NewSpeaker = profile.NewSpeaker,
+                GravatarUrl = profile.GravatarUrl()
+            };
+        }
+    }
+}
---
+++ DDDEastAnglia/Areas/Admin/Models/SpeakerModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace DDDEastAnglia.Areas.Admin.Models
+{
+    public class SpeakerModel
+    {
+        public int UserId { get; set; }
+
+        [DisplayName("username")]
+        public string UserName { get; set; }
+
+        [DisplayName("name")]
+        public string Name { get; set; }
+
+        [DisplayName("new speaker")]
+        public bool NewSpeaker { get; set; }
+
+        public string GravatarUrl { get; set; }
+
+        [DisplayName("submitted sessions")]
+        public int SubmittedSessionCount { get; set; }
+    }
+}
