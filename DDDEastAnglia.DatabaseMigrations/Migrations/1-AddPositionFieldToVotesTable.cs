c0bf42b24b3e1d62163d1d01befdb03af0ea6f42
Adrian Banks <github@adrianbanks.co.uk>
2014-06-19 23:18:42 +0100

    add a migration to add the voting position

---
+++ DDDEastAnglia.DatabaseMigrations/Migrations/1-AddPositionFieldToVotesTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace DDDEastAnglia.DatabaseMigrations.Migrations
+{
+    [Migration(20140619)]
+    public class AddPositionFieldToVotesTable : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Votes")
+                    .AddColumn("PosisionInList").AsInt32().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("PositionInList")
+                    .FromTable("Votes");
+        }
+    }
+}
