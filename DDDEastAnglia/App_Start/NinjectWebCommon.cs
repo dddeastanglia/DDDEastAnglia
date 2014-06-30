9ae063e60e23711b154e6e4dfb2babeb95ad3b29
Adrian Banks <adrian@grantadesign.com>
2014-06-30 15:53:11 +0100

    don't need this binding any more

--- DDDEastAnglia/App_Start/NinjectWebCommon.cs
+++ DDDEastAnglia/App_Start/NinjectWebCommon.cs
@@ -30,7 +30,6 @@ namespace DDDEastAnglia.App_Start
             var kernel = new StandardKernel();
             kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
             kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
-            kernel.Bind<IVotingCookie>().To<VotingCookie>();
             
             RegisterServices(kernel);
             return kernel;
