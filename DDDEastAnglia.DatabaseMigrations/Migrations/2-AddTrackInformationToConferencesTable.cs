c8f0f8c554fed0217c0ffb3b110aecbf16a04a8e
Adrian Banks <github@adrianbanks.co.uk>
2014-07-04 22:56:04 +0100

    add track information to the conferences table

---
+++ DDDEastAnglia.DatabaseMigrations/Migrations/2-AddTrackInformationToConferencesTable.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace DDDEastAnglia.DatabaseMigrations.Migrations
+{
+    [Migration(20140705)]
+    public class AddTrackInformationToConferencesTable : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Conferences")
+                    .AddColumn("NumberOfTimeSlots").AsInt32().NotNullable().WithDefaultValue(0)
+                    .AddColumn("NumberOfTracks").AsInt32().NotNullable().WithDefaultValue(0);
+        }
+
+        public override void Down()
+        {
+            Delete.Column("NumberOfTimeSlots")
+                    .Column("NumberOfTracks")
+                    .FromTable("Conferences");
+        }
+    }
+}
