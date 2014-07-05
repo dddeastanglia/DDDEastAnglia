11c91fd545cdf567ca1194b216dcfce95a9f3618
Adrian Banks <github@adrianbanks.co.uk>
2014-07-05 01:03:42 +0100

    Fix some broken tests

--- DDDEastAnglia.Tests/Helpers/Given_A_List_Of_Sessions.cs
+++ DDDEastAnglia.Tests/Helpers/Given_A_List_Of_Sessions.cs
@@ -33,25 +33,25 @@ namespace DDDEastAnglia.Tests.Helpers
         [Test]
         public void And_A_Conference_In_Voting_Then_Sorting_Should_Be_Random()
         {
-            var conference = Substitute.For<Conference>(1, "test-conference", "test");
+            var conference = Substitute.For<IConference>();
             conference.CanVote().Returns(true);
 
             _sorter.SortSessions(conference, _sessions);
 
-            _alphabeticalAlgorithm.DidNotReceive().Sort(Arg.Any<List<Models.SessionDisplayModel>>());
-            _randomAlgorithm.Received(1).Sort(Arg.Any<List<Models.SessionDisplayModel>>());
+            _alphabeticalAlgorithm.DidNotReceive().Sort(Arg.Any<List<SessionDisplayModel>>());
+            _randomAlgorithm.Received(1).Sort(Arg.Any<List<SessionDisplayModel>>());
         }
 
         [Test]
         public void And_A_Conference_Not_In_Voting_Then_Sorting_Should_Be_Alphabetical()
         {
-            var conference = Substitute.For<Conference>(1, "test-conference", "test");
+            var conference = Substitute.For<IConference>();
             conference.CanVote().Returns(false);
 
             _sorter.SortSessions(conference, _sessions);
 
-            _alphabeticalAlgorithm.Received(1).Sort(Arg.Any<List<Models.SessionDisplayModel>>());
-            _randomAlgorithm.DidNotReceive().Sort(Arg.Any<List<Models.SessionDisplayModel>>());
+            _alphabeticalAlgorithm.Received(1).Sort(Arg.Any<List<SessionDisplayModel>>());
+            _randomAlgorithm.DidNotReceive().Sort(Arg.Any<List<SessionDisplayModel>>());
         }
     }
 }
