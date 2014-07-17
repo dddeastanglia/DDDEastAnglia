875a1330a378b7007edbc276a52ec2496a245bc2
Adrian Banks <github@adrianbanks.co.uk>
2014-07-17 23:23:31 +0100

    split the common javascript out into a script file

---
+++ DDDEastAnglia/Scripts/expandCollapse.js
@@ -0,0 +1,24 @@
+﻿function expand(id, postUrl, postData) {
+    $.post(postUrl, postData)
+        .done(function (data) {
+            var loadingIcon = $('#icon' + id);
+            loadingIcon.replaceWith('<i id="icon' + id + '" class="icon-spin icon-spinner"></i>');
+            var output = $('#placeholder' + id);
+            output.hide();
+            output.html(data);
+            output.fadeIn('fast');
+            var link = $('#link' + id);
+            link.attr("onclick", "javascript:collapse('" + id + "', '" + postUrl + "', " + JSON.stringify(postData) + ");");
+            loadingIcon = $('#icon' + id);
+            loadingIcon.replaceWith('<i id="icon' + id + '" class="icon-chevron-down"></i>');
+        });
+}
+
+function collapse(id, postUrl, postData) {
+    var output = $('#placeholder' + id);
+    output.fadeOut('fast');
+    var link = $('#link' + id);
+    link.attr("onclick", "javascript:expand('" + id + "', '" + postUrl + "', " + JSON.stringify(postData) + "); return false;");
+    var loadingIcon = $('#icon' + id);
+    loadingIcon.replaceWith('<i id="icon' + id + '" class="icon-chevron-right"></i>');
+}
