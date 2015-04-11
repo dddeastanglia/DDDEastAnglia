38c6e7f282fbe08d78c3749eaf4b60672fa156fe
Adrian Banks <github@adrianbanks.co.uk>
2015-04-11 11:32:09 +0100

    Add schema change to introduce a sponsors table

---
+++ DDDEastAnglia.DatabaseMigrations/Migrations/3-AddSponsorsTable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace DDDEastAnglia.DatabaseMigrations.Migrations
+{
+    [Migration(20150411)]
+    public class AddSponsorsTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Sponsors")
+                .WithColumn("SponsorId").AsInt32().NotNullable().PrimaryKey()
+                .WithColumn("Name").AsString(50).NotNullable()
+                .WithColumn("Url").AsString(255).NotNullable()
+                .WithColumn("SponsorshipAmount").AsInt32().Nullable()
+                .WithColumn("Logo").AsBinary().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Sponsors");
+        }
+    }
+}
