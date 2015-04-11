c197c60bb4fbfef0e7a931228f0b430153d78cc4
Adrian Banks <github@adrianbanks.co.uk>
2015-04-11 15:58:20 +0100

    Add display annotations to the model

--- DDDEastAnglia/Areas/Admin/Models/SponsorModel.cs
+++ DDDEastAnglia/Areas/Admin/Models/SponsorModel.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace DDDEastAnglia.Areas.Admin.Models
 {
     public class SponsorModel
     {
         public int SponsorId { get; set; }
+
+        [DisplayName("Company Name")]
         public string Name { get; set; }
+
+        [DisplayName("Website Url")]
         public string Url { get; set; }
+
+        [DisplayName("Sponsorship Amount")]
         public int SponsorshipAmount { get; set; }
+
+        [DisplayName("Sponsorship Recieved On")]
         public DateTime? PaymentDate { get; set; }
     }
 }
