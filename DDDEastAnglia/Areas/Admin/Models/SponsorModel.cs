ec9e0f7e526284892a6d3ff36feda1c100c34c84
Adrian Banks <github@adrianbanks.co.uk>
2015-04-11 15:55:42 +0100

    Convert to auto-property

--- DDDEastAnglia/Areas/Admin/Models/SponsorModel.cs
+++ DDDEastAnglia/Areas/Admin/Models/SponsorModel.cs
@@ -4,11 +4,10 @@ namespace DDDEastAnglia.Areas.Admin.Models
 {
     public class SponsorModel
     {
-        private DateTime? paymentDate;
         public int SponsorId { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
         public int SponsorshipAmount { get; set; }
-        public DateTime? PaymentDate { get { return paymentDate; } set { paymentDate = value; } }
+        public DateTime? PaymentDate { get; set; }
     }
 }
