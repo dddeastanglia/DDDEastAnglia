68ecd0168b0dbeaaefe3ce9edca090b7eab8939d
Adrian Banks <github@adrianbanks.co.uk>
2015-04-13 23:12:18 +0100

    Add some tests for the image size contraining

---
+++ DDDEastAnglia.Tests/Helpers/ImageResizeExtensionsTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+using DDDEastAnglia.Helpers;
+using NUnit.Framework;
+
+namespace DDDEastAnglia.Tests.Helpers
+{
+    [TestFixture]
+    public sealed class ImageResizeExtensionsTests
+    {
+        [Test]
+        public void ContrainToHeight_DoesNotResizeTheSuppliedImage_WhenItIsShorterThanTheDesiredHeight()
+        {
+            const int originalHeight = 300;
+
+            using (var bitmap = new Bitmap(200, originalHeight))
+            {
+                const int newHeight = 400;
+                var resizedBitmap = bitmap.ContrainToHeightOf(newHeight);
+                Assert.That(resizedBitmap.Height, Is.EqualTo(originalHeight));
+            }
+        }
+
+        [Test]
+        public void ContrainToHeight_ResizesTheSuppliedImage_WhenItIsTallerThanTheDesiredHeight()
+        {
+            using (var bitmap = new Bitmap(200, 300))
+            {
+                const int newHeight = 20;
+                var resizedBitmap = bitmap.ContrainToHeightOf(newHeight);
+                Assert.That(resizedBitmap.Height, Is.EqualTo(newHeight));
+            }
+        }
+
+        [Test]
+        public void ContrainToWidth_DoesNotResizeTheSuppliedImage_WhenItIsNarrowerThanTheDesiredWidth()
+        {
+            const int originalWidth = 200;
+
+            using (var bitmap = new Bitmap(originalWidth, 300))
+            {
+                const int newWidth = 400;
+                var resizedBitmap = bitmap.ContrainToWidthOf(newWidth);
+                Assert.That(resizedBitmap.Width, Is.EqualTo(originalWidth));
+            }
+        }
+
+        [Test]
+        public void ContrainToWidth_ResizesTheSuppliedImage_WhenItIsWiderThanTheDesiredWidth()
+        {
+            using (var bitmap = new Bitmap(200, 300))
+            {
+                const int newWidth = 20;
+                var resizedBitmap = bitmap.ContrainToWidthOf(newWidth);
+                Assert.That(resizedBitmap.Width, Is.EqualTo(newWidth));
+            }
+        }
+    }
+}
