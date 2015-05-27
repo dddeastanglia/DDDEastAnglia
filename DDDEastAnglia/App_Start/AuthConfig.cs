f6a5d23128e617e71b7e4dc64646de56d6317bf2
Adrian Banks <github@adrianbanks.co.uk>
2015-05-27 20:34:21 +0100

    Wire in OAuth2 for Google

--- DDDEastAnglia/App_Start/AuthConfig.cs
+++ DDDEastAnglia/App_Start/AuthConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Configuration;
+using DotNetOpenAuth.GoogleOAuth2;
 using Microsoft.Web.WebPages.OAuth;
 
 namespace DDDEastAnglia
@@ -10,18 +11,20 @@ namespace DDDEastAnglia
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 
-            // NOTE: if you add/edit this list, be sure to edit the OAuth provider links in 
+            // NOTE: if you add/edit this list, be sure to edit the OAuth provider links in
             //          the menu and the registration page as they don't get picked up automatically
 
             OAuthWebSecurity.RegisterClient(new GitHubOAuthClient(
                 WebConfigurationManager.AppSettings["GitHubAppId"],
                 WebConfigurationManager.AppSettings["GitHubSecret"]), "GitHub", null);
-            
+
             OAuthWebSecurity.RegisterTwitterClient(
                 WebConfigurationManager.AppSettings["TwitterKey"],
                 WebConfigurationManager.AppSettings["TwitterSecret"]);
 
-            OAuthWebSecurity.RegisterGoogleClient();
+            OAuthWebSecurity.RegisterClient(new GoogleOAuth2Client(
+                WebConfigurationManager.AppSettings["GoogleClientId"],
+                WebConfigurationManager.AppSettings["GoogleSecret"]), "Google", null);
         }
     }
 }
