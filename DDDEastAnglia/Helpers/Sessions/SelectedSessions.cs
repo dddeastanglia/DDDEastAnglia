ccc5545fc6e0a6a35a23ebf8c0bcf88c8d7f0d8c
Adrian Banks <github@adrianbanks.co.uk>
2018-06-21 22:34:59 +0100

    Clear out the selected session and speaker ids

--- DDDEastAnglia/Helpers/Sessions/SelectedSessions.cs
+++ DDDEastAnglia/Helpers/Sessions/SelectedSessions.cs
@@ -4,62 +4,8 @@ namespace DDDEastAnglia.Helpers.Sessions
 {
     public static class SelectedSessions
     {
-        public static IEnumerable<int> SpeakerIds => new[]
-        {
-            8701,   // Joe Stead
-            34,     // Phil Trelford
-            8691,   // Joseph Woodward
-            298,    // Colin Angus Mackay
-            28,     // Ceri Shaw
-            8719,   // Galiya Warrier
-            2314,   // Sam Hogarth
-            8693,   // Robin Minto
-            8705,   // Paul McGrath
-            8516,   // Bart Read
-            5337,   // Matt Ellis
-            1,      // Ian Johnson
-            8718,   // Gary Short
-            53,     // Ian Russell
-            2313,   // Nathan Gloyn
-            2311,   // Ian Cooper
-            8703,   // Duncan Jones
-            8715,   // Karo Stoltzenburg
-            8707,   // Viv Richards
-            8717,   // Dylan Beattie
-            8576,   // Chris O'Dell
-            8699,   // Stuart Lang
-            8716,   // Benji Weber
-            6496,   // Steve Spencer
-            16      // Gary Ewan Park
-        };
+        public static IEnumerable<int> SpeakerIds => new int[0];
 
-        public static IEnumerable<int> SessionIds => new[]
-        {
-            2088,   // Joe Stead
-            2086,   // Phil Trelford
-            2072,   // Joseph Woodward
-            2094,   // Colin Angus Mackay
-            2081,   // Ceri Shaw
-            2125,   // Galiya Warrier
-            2092,   // Sam Hogarth
-            2078,   // Robin Minto
-            2095,   // Paul McGrath
-            2128,   // Bart Read
-            2122,   // Matt Ellis
-            2101,   // Ian Johnson
-            2123,   // Gary Short
-            2100,   // Ian Russell
-            2126,   // Nathan Gloyn
-            2085,   // Ian Cooper
-            2089,   // Duncan Jones
-            2115,   // Karo Stoltzenburg
-            2098,   // Viv Richards
-            2114,   // Dylan Beattie
-            2116,   // Chris O'Dell
-            2110,   // Stuart Lang
-            2111,   // Benji Weber
-            2090,   // Steve Spencer
-            2119    // Gary Ewan Park
-        };
+        public static IEnumerable<int> SessionIds => new int[0];
     }
 }
