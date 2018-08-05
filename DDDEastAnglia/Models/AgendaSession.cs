b4a7992880cc220e330f277c6d2b6b03e6fd3b94
Adrian Banks <github@adrianbanks.co.uk>
2018-08-05 22:43:59 +0100

    Simplify creating the agenda page
    It's still fairly manual, but it is now less error prone as only the
    session id needs to be used to render the session.

---
+++ DDDEastAnglia/Models/AgendaSession.cs
@@ -0,0 +1,11 @@
+﻿namespace DDDEastAnglia.Models
+{
+    public sealed class AgendaSession
+    {
+        public int SessionId { get; set; }
+        public string SessionTitle { get; set; }
+
+        public int SpeakerUserId { get; set; }
+        public string SpeakerName { get; set; }
+    }
+}
