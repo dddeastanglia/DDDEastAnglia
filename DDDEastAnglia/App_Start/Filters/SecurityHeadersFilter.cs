48241252dfa5311592969076fa9f98f72f455f5c
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-11-19 15:50:02 +0000

    ASPNET header removal refactored to private method

--- DDDEastAnglia/App_Start/Filters/SecurityHeadersFilter.cs
+++ DDDEastAnglia/App_Start/Filters/SecurityHeadersFilter.cs
@@ -17,6 +17,12 @@ namespace DDDEastAnglia.App_Start.Filters
             HttpContextBase contextBase = filterContext.HttpContext;
             HttpResponseBase responseBase = contextBase.Response;
             NameValueCollection headers = responseBase.Headers;
+
+            RemoveASPNETHeaders(headers);
+        }
+
+        private void RemoveASPNETHeaders(NameValueCollection headers)
+        {
             headers.Remove("x-powered-by");
             headers.Remove("x-aspnet-version");
             headers.Remove("x-aspnetmvc-version");
