2e5128da08c443255cc7d79d258271ac84788f72
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-11-19 15:39:09 +0000

    Remove X-Powered-By header

---
+++ DDDEastAnglia.Tests/Filters/SecurityHeadersFilterTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DDDEastAnglia.App_Start.Filters;
+using DDDEastAnglia.Areas.Admin.Models;
+using DDDEastAnglia.Controllers;
+using DDDEastAnglia.DataAccess;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DDDEastAnglia.Tests.Filters
+{
+
+    [TestFixture]
+    public class SecurityHeadersFilterTests
+    {
+        [Test]
+        public void XPoweredBy_Header_Is_Removed()
+        {
+            HttpContextBase contextBase = Substitute.For<HttpContextBase>();
+            HttpResponseBase responseBase = Substitute.For<HttpResponseBase>();
+            NameValueCollection headers = new NameValueCollection {{"X-Powered-By", "ASP.NET"}};
+            responseBase.Headers.Returns(headers);
+            contextBase.Response.Returns(responseBase);
+
+            ResultExecutingContext context = new ResultExecutingContext
+            {
+                HttpContext = contextBase
+            };
+
+            SecurityHeadersFilter filter = new SecurityHeadersFilter();
+
+            filter.OnResultExecuting(context);
+
+            NameValueCollection filteredHeaders = responseBase.Headers;
+            
+            Assert.That(filteredHeaders["X-Powered-By"], Is.Null);
+        }
+    }
+}
