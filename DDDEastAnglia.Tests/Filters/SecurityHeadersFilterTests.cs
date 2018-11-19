b8a5c38303edd4afb5100280e278236eb92398ae
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-11-19 17:06:12 +0000

    Refactor common code into Setup method

--- DDDEastAnglia.Tests/Filters/SecurityHeadersFilterTests.cs
+++ DDDEastAnglia.Tests/Filters/SecurityHeadersFilterTests.cs
@@ -15,21 +15,32 @@ namespace DDDEastAnglia.Tests.Filters
     [TestFixture]
     public class SecurityHeadersFilterTests
     {
+        private HttpContextBase contextBase;
+        private HttpResponseBase responseBase;
+        private NameValueCollection headers;
+        private ResultExecutingContext context;
+
+        [SetUp]
+        public void Setup()
+        {
+            contextBase = Substitute.For<HttpContextBase>();
+            responseBase = Substitute.For<HttpResponseBase>();
+            headers = new NameValueCollection();
+            responseBase.Headers.Returns(headers);
+            contextBase.Response.Returns(responseBase);
+
+            context = new ResultExecutingContext
+            {
+                HttpContext = contextBase
+            };
+        }
+
         [TestCase("x-powered-by", "ASP.NET")]
         [TestCase("x-aspnet-version", "4.0.30319")]
         [TestCase("x-aspnetmvc-version", "4.0")]
         public void ASPNET_Header_Is_Removed(string headerName, string headerValue)
         {
-            HttpContextBase contextBase = Substitute.For<HttpContextBase>();
-            HttpResponseBase responseBase = Substitute.For<HttpResponseBase>();
-            NameValueCollection headers = new NameValueCollection {{headerName, headerValue}};
-            responseBase.Headers.Returns(headers);
-            contextBase.Response.Returns(responseBase);
-
-            ResultExecutingContext context = new ResultExecutingContext
-            {
-                HttpContext = contextBase
-            };
+            headers.Add(headerName,headerValue);
 
             SecurityHeadersFilter filter = new SecurityHeadersFilter();
 
@@ -43,17 +54,6 @@ namespace DDDEastAnglia.Tests.Filters
         [Test]
         public void Security_Header_Is_Added()
         {
-            HttpContextBase contextBase = Substitute.For<HttpContextBase>();
-            HttpResponseBase responseBase = Substitute.For<HttpResponseBase>();
-            NameValueCollection headers = new NameValueCollection();
-            responseBase.Headers.Returns(headers);
-            contextBase.Response.Returns(responseBase);
-
-            ResultExecutingContext context = new ResultExecutingContext
-            {
-                HttpContext = contextBase
-            };
-
             SecurityHeadersFilter filter = new SecurityHeadersFilter();
 
             filter.OnResultExecuting(context);
@@ -66,17 +66,6 @@ namespace DDDEastAnglia.Tests.Filters
         [Test]
         public void Security_Header_Is_Correct_Value()
         {
-            HttpContextBase contextBase = Substitute.For<HttpContextBase>();
-            HttpResponseBase responseBase = Substitute.For<HttpResponseBase>();
-            NameValueCollection headers = new NameValueCollection();
-            responseBase.Headers.Returns(headers);
-            contextBase.Response.Returns(responseBase);
-
-            ResultExecutingContext context = new ResultExecutingContext
-            {
-                HttpContext = contextBase
-            };
-
             SecurityHeadersFilter filter = new SecurityHeadersFilter();
 
             filter.OnResultExecuting(context);
