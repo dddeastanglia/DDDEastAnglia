a5fed45701d58eb197951ba15a4490661015b33e
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-11-19 15:39:09 +0000

    Remove X-Powered-By header

---
+++ DDDEastAnglia/App_Start/Filters/SecurityHeadersFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DDDEastAnglia.App_Start.Filters
+{
+    public class SecurityHeadersFilter : IResultFilter
+    {
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            HttpContextBase contextBase = filterContext.HttpContext;
+            HttpResponseBase responseBase = contextBase.Response;
+            NameValueCollection headers = responseBase.Headers;
+            headers.Remove("X-Powered-By");
+        }
+    }
+}
