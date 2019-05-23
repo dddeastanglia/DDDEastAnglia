230f7a959de6cfdf814ed64751059b7635cbca46
Phil Pursglove <philpursglove@users.noreply.github.com>
2019-05-23 21:52:38 +0100

    Add DB Migration to add Anonymous Sessions property to the Conferences table

---
+++ DDDEastAnglia.DatabaseMigrations/Migrations/4-AddAnonymousSessionsSetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace DDDEastAnglia.DatabaseMigrations.Migrations
+{
+    [Migration(20190523)]
+    public class AddAnonymousSessionsSetting : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("Conferences")
+                .AddColumn("AnonymousSessions").AsBoolean().Nullable();
+        }
+    }
+}
