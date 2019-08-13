1ca926dd0b25a8cb1efa08585bac8bfeb4af2fa5
Adrian Banks <github@adrianbanks.co.uk>
2019-08-13 23:08:26 +0100

    Fix the display of session and speaker information from the agenda

--- DDDEastAnglia/Controllers/SessionController.cs
+++ DDDEastAnglia/Controllers/SessionController.cs
@@ -72,7 +72,7 @@ namespace DDDEastAnglia.Controllers
             var userProfile = userProfileRepository.GetUserProfileByUserName(session.SpeakerUserName);
 
             var conference = conferenceLoader.LoadConference();
-            var showSpeaker = conference.CanShowSpeakers();
+            var showSpeaker = conference.CanShowSpeakers() || conference.CanPublishAgenda();
 
             var displayModel = CreateDisplayModel(session, userProfile, showSpeaker);
 
